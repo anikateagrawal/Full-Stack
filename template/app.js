6ee9492013dc9d47ed236367209db54f65d25f1d
anikateagrawal <anikateag7316@gmail.com>
2023-01-25 11:27:56 +0530

    commit

--- template/app.js
+++ template/app.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app=express();
 const port=8080;
+const path=require("path");
+
+app.set('views',path.join(__dirname,'views'));
 
 app.listen(port,()=>{
     console.log("server is running at port ",port);
@@ -9,5 +12,7 @@ app.listen(port,()=>{
 app.set('view engine','ejs');
 
 app.get('/home',(req,res)=>{
-    res.render('index');
+
+    rn=Math.floor(Math.random()*10);
+    res.render('index',{rn});
 })
